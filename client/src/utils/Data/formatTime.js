fe27d89ce0544be9922059dbafdf409297eb4495
Musab Zahid Ansari <musabzahida@gmail.com>
2025-07-27 00:18:50 +0530

    Revert "Train-Schedule-Feature"
    This reverts commit 2ec70429137c21d78e575ba91b066561656c6b61.

--- client/src/utils/Data/formatTime.js
+++
@@ -1,8 +0,0 @@
-const formatTime = (hour, minute) => {
-  const ampm = hour >= 12 ? "PM" : "AM";
-  const formattedHour = hour % 12 === 0 ? 12 : hour % 12;
-  const formattedMinute = minute < 10 ? `0${minute}` : minute;
-  return `${formattedHour}:${formattedMinute} ${ampm}`;
-};
-
-export default formatTime;
